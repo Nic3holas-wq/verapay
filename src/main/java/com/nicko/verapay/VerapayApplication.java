483a7cead850bdd39e6fe636d0099b34d035d207
Nicholas <nicholasmurimi254@gmail.com>
2026-06-03 10:42:55 +0300

    Initial commit

---
+++ src/main/java/com/nicko/verapay/VerapayApplication.java
@@ -0,0 +1,13 @@
+package com.nicko.verapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicko/verapay/VerapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicko.verapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
