9341b7f0500f6698dfc3e5cea5627b94172471ea
Nicholas <nicholasmurimi254@gmail.com>
2026-06-07 09:34:44 +0300

    add authentication using JWT and authorization

---
+++ src/main/java/com/nicko/verapay/audit/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.nicko.verapay.audit;
+
+import com.nicko.verapay.util.ApplicationUtility;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorAwareImpl")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(ApplicationUtility.getLoggedInUser());
+    }
+}
