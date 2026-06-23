1e32e8c71b6e92a4cb0761f7fcec3a3c8dd6edb8
Nicholas <nicholasmurimi254@gmail.com>
2026-06-23 22:36:04 +0300

    add github actions CI/CD pipeline 5

--- src/test/java/com/nicko/verapay/VerapayApplicationTests.java
+++ src/test/java/com/nicko/verapay/VerapayApplicationTests.java
@@ -1,6 +1,5 @@
 package com.nicko.verapay;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-@Tag("integration")
 class VerapayApplicationTests {
 
 	@Test
